717f7e768e967358f957dbee9312c6d38c7fb3e3
20180073 <20180073@kumoh.ac.kr>
2021-08-02 03:06:52 +0900

    input time

--- app/src/main/java/kr/ac/kumoh/s20180073/myapplication/ui/day/DayFragment.java
+++ app/src/main/java/kr/ac/kumoh/s20180073/myapplication/ui/day/DayFragment.java
@@ -1,16 +1,26 @@
 package kr.ac.kumoh.s20180073.myapplication.ui.day;
 
+import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.sql.Time;
+import java.util.Calendar;
 
 import kr.ac.kumoh.s20180073.myapplication.R;
 
@@ -23,13 +33,24 @@ public class DayFragment extends Fragment {
         dayViewModel =
                 ViewModelProviders.of(this).get(DayViewModel.class);
         View root = inflater.inflate(R.layout.fragment_day, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dayViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        //final TextView textView = root.findViewById(R.id.text_dashboard);
+        //dayViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            //@Override
+            //public void onChanged(@Nullable String s) {
+                //textView.setText(s);
+            //}
+        //});
+
+        FloatingActionButton addButton = root.findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), TimeActivity.class);
+                startActivityForResult(intent, 101);
+                //startActivity(new Intent(getContext(), TimeActivity.class));
             }
         });
+
         return root;
     }
 }
---
+++ app/src/main/java/kr/ac/kumoh/s20180073/myapplication/ui/day/TimeActivity.java
@@ -0,0 +1,135 @@
+package kr.ac.kumoh.s20180073.myapplication.ui.day;
+
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+import kr.ac.kumoh.s20180073.myapplication.R;
+
+public class TimeActivity extends AppCompatActivity {
+
+    private DayViewModel dayViewModel;
+
+    TextView startTime;
+    TextView endTime;
+    boolean inputStart = false;
+    boolean inputEnd = false;
+    int starthour, startminute, endhour, endeminute, gaphour, gapminute;
+    String select;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time);
+
+        dayViewModel = ViewModelProviders.of(this).get(DayViewModel.class);
+
+        startTime = findViewById(R.id.startTime);
+        startTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setTime();
+                inputStart = true;
+            }
+        });
+
+        endTime = findViewById(R.id.endTime);
+        endTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setTime();
+                inputEnd = true;
+            }
+        });
+
+        final Spinner workSpinner = findViewById(R.id.workSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinner_work, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        workSpinner.setAdapter(adapter);
+
+        workSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                select = workSpinner.getSelectedItem().toString();
+                Toast.makeText(TimeActivity.this, select, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(TimeActivity.this, "일정을 선택 해 주세요!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button saveButton = findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //저장버튼 눌렀을 때 아마 디비에 저장?
+            }
+        });
+    }
+
+    void setTime(){
+        final Calendar c = Calendar.getInstance();
+        int mHour = c.get(Calendar.HOUR);
+        int mMinute = c.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+                if(inputStart == true) {
+                    startTime.setText(String.format("%02d:%02d", hour, minute));
+                    starthour = hour;
+                    startminute = minute;
+                    inputStart = false;
+                }
+                else if(inputEnd==true) {
+                    endTime.setText(String.format("%02d:%02d", hour, minute));
+                    endhour = hour;
+                    endeminute = minute;
+                    inputEnd = false;
+                    setGap();
+                }
+            }
+        }, mHour, mMinute, false);
+        timePickerDialog.show();
+    }
+
+    void setGap(){
+        TextView gap = findViewById(R.id.gap);
+
+        if(endhour >= starthour){
+            gaphour = endhour - starthour;
+            gapminute = endeminute - startminute;
+
+            if(endhour == starthour) {
+                if (endeminute >= startminute) {
+                    gap.setText(String.format("%02d:%02d", gaphour, gapminute));
+                }
+            }
+            else{
+                gap.setText(String.format("%02d:%02d", gaphour, gapminute));
+
+                if (endeminute < startminute){
+                    gaphour = gaphour - 1;
+                    gapminute = 60 - Math.abs(endeminute - startminute);
+                    gap.setText(String.format("%02d:%02d", gaphour, gapminute));
+                }
+            }
+        }
+    }
+
+}
